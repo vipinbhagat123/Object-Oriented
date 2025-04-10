0c19b1b59a4ae7aca2c6590f9f3e653c4c7f9253
vipinbhagat123 <bhagatvipin59@gmail.com>
2025-04-10 13:59:15 +0530

    class only

---
+++ oopss/3.py
@@ -0,0 +1,9 @@
+# python is high level interpreted programming language used for web development
+# machine learning and ai related fields
+
+# create a class
+
+class MyClass:      #class
+    x=5
+p1=MyClass()        # object
+print(p1.x)
