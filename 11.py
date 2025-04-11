e434147b13bed1ffa03f126228992852fd923888
Vipin Bhagat <108531710+vipinbhagat123@users.noreply.github.com>
2025-04-11 20:55:02 +0530

    The pass Statement

---
+++ 11.py
@@ -0,0 +1,7 @@
+# The pass statement
+
+# class definitions cannot be empty, but if you for some reason have a class
+# definition with no content, put in the pass statement to avoid getting an error
+
+class person:
+    pass
