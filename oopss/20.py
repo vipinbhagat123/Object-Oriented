7092cd609d2f8ff55d2e5f755e160457c533ec91
Vipin Bhagat <108531710+vipinbhagat123@users.noreply.github.com>
2025-04-12 00:43:44 +0530

    iter

---
+++ oopss/20.py
@@ -0,0 +1,19 @@
+class mynumber:
+    def __iter__(self):
+        self.a = 1
+        return self
+
+    def __next__(self):
+        x=self.a
+        self.a+=1
+        return x
+myclass = mynumber()
+myiter = iter(myclass)
+
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+
+
