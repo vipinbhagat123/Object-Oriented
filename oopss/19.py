f2af33ce8f48973b8c46bbba86d081ad75c8e33a
Vipin Bhagat <108531710+vipinbhagat123@users.noreply.github.com>
2025-04-12 00:37:39 +0530

    iter

---
+++ oopss/19.py
@@ -0,0 +1,15 @@
+mytuple = ('apple', 'banana', 'cherry')
+myit = iter(mytuple)
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+
+mystr='banana'
+m=iter(mystr)
+print(next(m))
+print(next(m))
+print(next(m))  
+print(next(m))
+print(next(m))
+print(next(m))
