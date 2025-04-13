3cd5650771f7c2fdd5b1ea76c2848e570669b1f7
Vipin Bhagat <108531710+vipinbhagat123@users.noreply.github.com>
2025-04-13 23:59:39 +0530

    Polymorphism

---
+++ oops( Chai Aur Code)/5.Polymorphism.py
@@ -0,0 +1,32 @@
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+    
+    def full_name(self):
+        return f"{self.brand} {self.model}"
+
+    def fuel_type(self):
+        return "Petrol or Disel"
+
+class ElectricCar(Car):
+    def __init__(self, brand, model, battery_size):
+        super().__init__(brand, model)
+        self.battery_size = battery_size
+    
+    def full_name(self):
+        return f"{self.brand} {self.model} with battery size {self.battery_size}"
+    
+    def fuel_type(self):
+        return "Electric Charge"
+k=ElectricCar("Tesla", "Model S", "100kWh")
+l=Car("Toyota", "Corolla")
+print(l.brand) 
+print(l.model)
+print(l.fuel_type())
+
+print(k.brand)
+print(k.model)
+print(k.battery_size)
+print(k.full_name())
+print(k.fuel_type())
