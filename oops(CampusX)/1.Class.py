8c69fa978f0b59479e46c7aa37528c7b3ec3ddfa
Vipin Bhagat <108531710+vipinbhagat123@users.noreply.github.com>
2025-04-17 14:06:10 +0530

    Basic Class

--- oops(CampusX)/1.Class.py
+++ oops(CampusX)/1.Class.py
@@ -5,3 +5,11 @@
 # snake_case
 # Ex : snake_case #where we use the _ for that we called snake_case.
 
+#class name should always be in pascal Case
+class Car:
+    color = "blue" #data
+    model = 'sports' #data
+
+    # variable name should alway be in snake case.
+    def calculate_avg_speed(km, time):
+        #Some Code
