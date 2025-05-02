fde60a3a3eda07e8f97f25be92bab741a7803167
vipinbhagat123 <bhagatvipin59@gmail.com>
2025-05-02 08:37:37 +0530

    oops definition and creating a class

---
+++ oops(geeks_for_geeks)/1.py
@@ -0,0 +1,20 @@
+# Object Oriented Programminng is a fundamental concept in python, empowering developers to build modular, mainatainable, and scalable applications.
+
+#  OOPs Concepts in Python
+#  Class in Python
+#  Objects in Python
+#  Polymorphism in Python
+#  Encapsulation in Python
+#  Inheritance in Python
+#  Data Abstraction in Python
+
+# Class in python
+
+# Classes are blueprints fro creating objects. A class defines a set of attributes and methods that the created objects can have
+
+class Dog:
+    specieies = 'Canine'    # class attrubute
+
+    def __init__(self, name, age):
+        self.name = name    # Instance attribute
+        self.age = age      # Instance attribute
