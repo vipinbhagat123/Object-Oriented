b282beaa27b09ec9b806d996d320778ad3a41449
vipinbhagat123 <bhagatvipin59@gmail.com>
2025-05-02 11:35:26 +0530

    class and instance variable

--- oops(geeks_for_geeks)/5.py
+++ oops(geeks_for_geeks)/5.py
@@ -36,3 +36,11 @@ print(dog1.name)    # (updated instance variable)
 Dog.species = 'Feline'
 print(dog1.species) # (updated class variable)
 print(dog2.species)
+
+
+Explanation:
+
+#  Class Variable (species): Shared by all instances of the class. Changing Dog.species affects all objects, as it’s a property of the class itself.
+#  Instance Variables (name, age): Defined in the __init__ method. Unique to each instance (e.g., dog1.name and dog2.name are different).
+#  Accessing Variables: Class variables can be accessed via the class name (Dog.species) or an object (dog1.species). Instance variables are accessed via the object (dog1.name).
+#  Updating Variables: Changing Dog.species affects all instances. Changing dog1.name only affects dog1 and does not impact dog2.
