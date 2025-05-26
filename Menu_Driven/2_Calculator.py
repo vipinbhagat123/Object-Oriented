5ec6494070470c9d76611ba36ce700fdff2a0561
Vipin Bhagat <108531710+vipinbhagat123@users.noreply.github.com>
2025-05-26 11:09:19 +0530

    Calculator

--- Menu_Driven/2_Calculator.py
+++ Menu_Driven/2_Calculator.py
@@ -21,3 +21,42 @@ def c_modulo(a, b):
     print("Modulus of A and B:", k)
 
 print("Calculator for Addition, Substraction, Multiplication, Division and Modulus")
+
+
+while True:
+    print('1. Addition.')
+    print('2. Substraction.')
+    print('3. Multiplication.')
+    print('4. Division.')
+    print('5. Modulus.')
+    
+    choice=int(input("\nEnter the option :"))
+    if choice==1:
+        a=int(input("\nEnter the value of a:"))
+        b=int(input("\nEnter the value of b:"))
+        c_add(a, b)
+    
+    elif choice==2:
+        a=int(input("\nEnter the value of a:"))
+        b=int(input("\nEnter the value of b:"))
+        c_substract(a, b)
+    
+    elif choice==3:
+        a=int(input("\nEnter the value of a:"))
+        b=int(input("\nEnter the value of b:"))
+        c_multiplication(a, b)
+    elif choice==4:
+        a=int(input("\nEnter the value of a:"))
+        b=int(input("\nEnter the value of b:"))
+        c_division(a, b)
+
+    elif choice==5:
+        a=int(input("\nEnter the value of a:"))
+        b=int(input("\nEnter the value of b:"))
+        c_modulo(a, b)
+    
+    elif choice==6:
+        print("Thank you! please come again.")
+        break
+    else:
+        print('Incorrect Choice.')
