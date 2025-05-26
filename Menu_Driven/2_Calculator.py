f5449a79ad76c23ce0657190d83556a55f2c3bed
vipinbhagat123 <bhagatvipin59@gmail.com>
2025-05-26 10:31:52 +0530

    Calculator

---
+++ Menu_Driven/2_Calculator.py
@@ -0,0 +1,23 @@
+# Menu driven program for calculator where we add, divide, multiply and substract
+
+def c_add(a,b):
+    k=a+b
+    print("Addition of A and B:", k)
+
+def c_substract(a, b):
+    k=a-b
+    print("Substraction of A and B:", k)
+
+def c_multiplication(a, b):
+    k=a*b
+    print("Multiplication of A and B:", k)
+
+def c_division(a, b):
+    k=a/b
+    print("Divison of A and B:", k)
+
+def c_modulo(a, b):
+    k=a//b
+    print("Modulus of A and B:", k)
+
+print("Calculator for Addition, Substraction, Multiplication, Division and Modulus")
