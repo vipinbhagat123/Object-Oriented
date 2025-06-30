2cdc80562229226d08d417facba2d57dbc3dcc05
Vipin Bhagat <108531710+vipinbhagat123@users.noreply.github.com>
2025-06-30 23:57:43 +0530

    Update 1. Basic Class And Object.py

--- oops( Chai Aur Code)/1. Basic Class And Object.py
+++ oops( Chai Aur Code)/1. Basic Class And Object.py
@@ -1,5 +1,5 @@
 # problem : Create a car class with attributes like brand and model. Then create an instance of this class.
-  
+    
 class Car:
     def __init__(self, brand, model):
         self.brand = brand
